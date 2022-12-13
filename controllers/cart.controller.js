e4c5e6a72b3b07a72f240dca0dbf12c47374c330
Mohamed Tallal <tallal@sulfah.com>
2022-12-13 09:13:44 +0200

    cart is finshed

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,11 +1,9 @@
-const { useAttrs } = require("vue");
 const Cart = require("../models/carts.model")
 const Product = require("../models/product.model")
 
 exports.addToCart = (req ,res , next) => {
     let productId = req.params.id ; 
     let userId    = req.session.userId;
-    console.log("productId: " + productId)
     console.log("userId: " + userId)
 
     Product.getProductById(productId).then((data) => {
@@ -32,11 +30,56 @@ exports.getCart = (req , res, next) => {
     console.log('auth user data : ' + userId)
     Cart.getUserCart(userId).then((cartData) => {
         console.log('user cart data' +cartData);
+        let SuccesDeleteProduct = req.flash('succesProduct')
+        let ErrorDeleteProduct  =  req.flash('errorProduct')
         res.render('cart.ejs' , {
             cartData : cartData,
             auth : authData , 
+            SuccesDeleteProduct  : SuccesDeleteProduct , 
+            ErrorDeleteProduct :  ErrorDeleteProduct
         });
     }).catch(err => {
         console.log(err)
+        res.redirect('back')
+    })
+}
+
+exports.UpdateProductCart = (req ,res , next) => {
+    let cartId = req.params.id
+    let productAmount = req.body.amount 
+    Cart.updateProduct(cartId ,productAmount ).then((data) => {
+        req.flash('succesProduct' , data)
+        res.redirect('back')    
+    }).catch(err => {
+        req.flash('errorProduct' , err)
+        res.redirect('back')
+    })
+
+
+}
+
+exports.deleteProductCart = (req ,res , next) => {
+    let cartId = req.params.id
+    console.log('cart id: ' + cartId)
+    Cart.deleteProductCart(cartId).then((data) => {
+        req.flash('succesDeleteProduct' , data)
+        res.redirect('back')
+    }).catch((err) => {
+        req.flash('errorDeleteProduct' , err)
+        res.redirect('back')
+    })
+}
+
+exports.deleteAllProductCart = (req ,res , next) => {
+    let userId = req.session.userId
+    console.log('user id: ' + userId)
+    Cart.deleteAllProductCart(userId).then((data) => {
+        req.flash('succesDeleteProduct' , data)
+        console.log(data)
+        res.redirect('back')
+    }).catch((err) => {
+        req.flash('errorDeleteProduct' , err)
+        console.log(err)
+        res.redirect('back')
     })
 }
--- routes/cart.router.js
+++ routes/cart.router.js
@@ -1,9 +1,13 @@
 const router = require('express').Router()
 const cartController = require('../controllers/cart.controller');
-const AuthMiddleware = require('../middelware/auth.middelware')
-router.get('/cart' , AuthMiddleware.isUser , cartController.getCart)
-router.post('/cart/:id' , AuthMiddleware.isUser,cartController.addToCart)
-router.post('/cart/:id/update/:productId' , AuthMiddleware.isUser,cartController.UpdateProductCart)
-router.post('/cart/:id/destroy/:productId' , AuthMiddleware.isUser,cartController.deleteProductCart)
+const AuthMiddleware = require('../middelware/auth.middelware');
+const  bodyParser  = require("body-parser");
+const bodyParserMw = bodyParser.urlencoded({extended: true})
+ 
+router.get('/cart' , bodyParserMw , AuthMiddleware.isUser , cartController.getCart)
+router.post('/cart/:id' , bodyParserMw , AuthMiddleware.isUser,cartController.addToCart)
+router.post('/cart/:id/update/' , bodyParserMw , AuthMiddleware.isUser,cartController.UpdateProductCart)
+router.post('/cart/:id/destroy/' , bodyParserMw , AuthMiddleware.isUser,cartController.deleteProductCart)
+router.post('/cart/destroy/all/' , bodyParserMw , AuthMiddleware.isUser,cartController.deleteAllProductCart)
 
 module.exports = router
